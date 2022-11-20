ad1205bbeff542a244d772c7fb05bbffad0f99a5
LuisGa24 <69638645+LuisGa24@users.noreply.github.com>
2022-11-19 20:35:11 -0600

    initial commit

---
+++ src/main/java/ucr/ac/cr/sigiealumniapi/SigieAlumniApiApplication.java
@@ -0,0 +1,13 @@
+package ucr.ac.cr.sigiealumniapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigieAlumniApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigieAlumniApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ucr/ac/cr/sigiealumniapi/SigieAlumniApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ucr.ac.cr.sigiealumniapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigieAlumniApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
