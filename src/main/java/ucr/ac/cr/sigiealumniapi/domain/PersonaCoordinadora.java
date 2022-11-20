cfae6ff8c3bb6177b91f15267c68ce1f0983cd58
LuisGa24 <69638645+LuisGa24@users.noreply.github.com>
2022-11-20 14:43:23 -0600

    actualizacion modelo domino
    Se agregó el código necesario para administrar los ciclos de referencias circulares.

--- src/main/java/ucr/ac/cr/sigiealumniapi/domain/PersonaCoordinadora.java
+++ src/main/java/ucr/ac/cr/sigiealumniapi/domain/PersonaCoordinadora.java
@@ -1,5 +1,8 @@
 package ucr.ac.cr.sigiealumniapi.domain;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
@@ -16,9 +19,77 @@ public class PersonaCoordinadora {
     private Date fechaInicio;
 
     @OneToOne(mappedBy = "coordinador")
+    @JsonIdentityInfo(
+            generator = ObjectIdGenerators.PropertyGenerator.class,
+            property = "id") //Evita los ciclos de reperencias circulares
     private PlanEstudio planEstudio;
 
     public PersonaCoordinadora() {
     }
 
+    public PersonaCoordinadora(String id, String nombre, String apellidos, String correoInstitucional, String correoPersonal, Date fechaInicio, PlanEstudio planEstudio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correoInstitucional = correoInstitucional;
+        this.correoPersonal = correoPersonal;
+        this.fechaInicio = fechaInicio;
+        this.planEstudio = planEstudio;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreoInstitucional() {
+        return correoInstitucional;
+    }
+
+    public void setCorreoInstitucional(String correoInstitucional) {
+        this.correoInstitucional = correoInstitucional;
+    }
+
+    public String getCorreoPersonal() {
+        return correoPersonal;
+    }
+
+    public void setCorreoPersonal(String correoPersonal) {
+        this.correoPersonal = correoPersonal;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public PlanEstudio getPlanEstudio() {
+        return planEstudio;
+    }
+
+    public void setPlanEstudio(PlanEstudio planEstudio) {
+        this.planEstudio = planEstudio;
+    }
 }
--- src/main/java/ucr/ac/cr/sigiealumniapi/domain/PersonaGraduada.java
+++ src/main/java/ucr/ac/cr/sigiealumniapi/domain/PersonaGraduada.java
@@ -1,5 +1,8 @@
 package ucr.ac.cr.sigiealumniapi.domain;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -16,12 +19,111 @@ public class PersonaGraduada {
     private String correo;
 
     @ManyToMany(mappedBy = "personasGraduadas")
+    @JsonIdentityInfo(
+            generator = ObjectIdGenerators.PropertyGenerator.class,
+            property = "id") //Evita los ciclos de reperencias circulares
     private List<Recinto> recintos;
 
     @ManyToMany(mappedBy = "personasGraduadas")
+    @JsonIdentityInfo(
+            generator = ObjectIdGenerators.PropertyGenerator.class,
+            property = "id") //Evita los ciclos de reperencias circulares
     private List<PlanEstudio> carreras;
 
+    public PersonaGraduada(String id, String nombre, String apellidos, int anoGraduacion, String numCarne, Number promedioPonderado, String telefono, String correo, List<Recinto> recintos, List<PlanEstudio> carreras) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.anoGraduacion = anoGraduacion;
+        this.numCarne = numCarne;
+        this.promedioPonderado = promedioPonderado;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.recintos = recintos;
+        this.carreras = carreras;
+    }
 
+    public PersonaGraduada() {
 
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getAnoGraduacion() {
+        return anoGraduacion;
+    }
+
+    public void setAnoGraduacion(int anoGraduacion) {
+        this.anoGraduacion = anoGraduacion;
+    }
+
+    public String getNumCarne() {
+        return numCarne;
+    }
+
+    public void setNumCarne(String numCarne) {
+        this.numCarne = numCarne;
+    }
+
+    public Number getPromedioPonderado() {
+        return promedioPonderado;
+    }
+
+    public void setPromedioPonderado(Number promedioPonderado) {
+        this.promedioPonderado = promedioPonderado;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public List<Recinto> getRecintos() {
+        return recintos;
+    }
+
+    public void setRecintos(List<Recinto> recintos) {
+        this.recintos = recintos;
+    }
+
+    public List<PlanEstudio> getCarreras() {
+        return carreras;
+    }
+
+    public void setCarreras(List<PlanEstudio> carreras) {
+        this.carreras = carreras;
+    }
 }
